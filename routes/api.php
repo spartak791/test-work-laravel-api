774a621d904718d1e4d103fcbeb02118d90396b4
Spartak <fake@email.net>
2026-01-22 15:20:34 +0500

    Custom not found message (comment fix)

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\TaskController;
 use App\Http\Middleware\EnsureJsonRequest;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 Route::apiResource('tasks', TaskController::class)
     ->middlewareFor(
         ['store', 'update'],
         [EnsureJsonRequest::class]
-    );
+    )
+    ->missing(function () {
+        throw new NotFoundHttpException('Resource not found.');
+    });
